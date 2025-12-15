659febc898fe5add741d92c6396d7e935c634c87
Huss Hegazy <husshegazydev@gmail.com>
2025-12-14 21:45:13 -0500

    ZSH plugins minimal setup tsov

---
+++ zsh/install-plugins.zsh
@@ -0,0 +1,8 @@
+# Installs zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Installs zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Installs zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
